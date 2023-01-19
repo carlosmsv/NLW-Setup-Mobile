b4f5f21bf8fec60e0651626b82169dfe41a38ec3
Carlos <carlos.msv@hotmail.com>
2023-01-19 02:26:31 -0300

    styling week days letters using DAY_SIZE (square size)

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -10,12 +10,13 @@ export function Home(){
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
 
-      <View> 
+      <View className="flex-row mt-6 mb-2"> 
         { 
           weekDays.map((weekDay, i) => (
             <Text 
               key={`${weekDay}-${i}`}
               className="text-zinc-400 text-xl font-bold text-center mx-1"
+              style={{ width: DAY_SIZE }}
             >
               {weekDay}
             </Text>
