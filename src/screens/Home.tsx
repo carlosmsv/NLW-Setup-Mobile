8216fe9636b1d6474f4d433b803ce2637d65706a
Carlos <carlos.msv@hotmail.com>
2023-01-19 02:24:53 -0300

    showing week days (D, S, T, Q, Q, S)

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,27 @@
-import { View } from "react-native";
-import { HabitDay } from "../components/HabitDay";
+import { View, Text } from "react-native";
+
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+
+import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
 
 export function Home(){
   return(
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
+
+      <View> 
+        { 
+          weekDays.map((weekDay, i) => (
+            <Text 
+              key={`${weekDay}-${i}`}
+              className="text-zinc-400 text-xl font-bold text-center mx-1"
+            >
+              {weekDay}
+            </Text>
+          ))
+        }
+      </View>
       <HabitDay />
     </View>
   )
